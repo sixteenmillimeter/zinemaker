64e5fc79792d4c2d50d3bd7d0868e0752bd92545
mmcwilliams <hi@mmcwilliams.com>
2020-11-14 23:02:45 -0500

    Parse stdout from standard and get an "error" count to quickly enumerate messages. Still returning 25 errors right now.
    "25 errors found using standard"

--- lint.sh
+++ lint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 output=`standard --fix zine.js`
+errorCount=`echo "${output}" | wc -l`
 echo "${output}"
+
+msg="errors"
+if [ $errorCount == 1 ]; then
+	msg="error"
+fi
+echo "${errorCount} ${msg} found using standard"
